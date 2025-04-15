f1d902b63de56977d823b65392ae427e8025b46b
Mateusz Cichos <mateusz.cichos@edu.uekat.pl>
2025-04-15 23:14:38 +0200

    Create models and views

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,7 +2,14 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+class PublishedManager(models.Manager):
+	def get_queryset(self):
+		return super(PublishedManager, self).get_queryset()\
+											.filter(status='published')
+
 class Post(models.Model):
+    objects = models.Manager()
+    published = PublishedManager()
     STATUS_CHOICES = (
         ('draft', 'Draft'),
         ('published', 'Published'),
@@ -20,4 +27,6 @@ class Post(models.Model):
         ordering = ('-publish',)
     def __str__(self):
         return self.title
-# Create your models here.
+
+    
+
