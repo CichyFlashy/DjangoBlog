79497949a22216b8326c50073d444f6b1dc37745
Mateusz Cichos <mateusz.cichos@edu.uekat.pl>
2025-04-29 00:07:20 +0200

    Create form for comments, add it to view and templates

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from .models import Post
+from .models import Post, Comment
+from .forms import EmailPostForm, CommentForm
 from django.views.generic import ListView
-from .forms import EmailPostForm
 from django.core.mail import send_mail
 
 class PostListView(ListView):
@@ -30,7 +30,22 @@ def post_detail(request, year, month, day, post):
 								   publish__year=year,
 								   publish__month=month,
 								   publish__day=day)
-	return render(request, 'blog/post/detail.html', {'post':post})
+	#Lista aktywnych komentarzy dla danego posta
+	comments = post.comments.filter(active=True)
+
+	if request.method == 'POST':
+		#komentarz został opublikowany
+		comment_form = CommentForm(data=request.POST)
+		if comment_form.is_valid():
+			#Utworzenie obiektu Comment, ale jeszcze nie zapisujemy go w bazie danych
+			new_comment = comment_form.save(commit=False)
+			#Przypisanie komentarza do bieżącego posta
+			new_comment.post = post
+			#Zapisanie komentarza w bazie danych
+			new_comment.save()
+	else:
+		comment_form = CommentForm()
+	return render(request, 'blog/post/detail.html', {'post':post, 'comments':comments, 'comment_form':comment_form})
 
 
 def post_share(request, post_id):
